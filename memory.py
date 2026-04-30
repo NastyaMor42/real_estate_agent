0b8ab708f39784e2529099e17b226245b8202586
NastyaMor42 <88672490+NastyaMor42@users.noreply.github.com>
2026-04-30 18:18:59 +0300

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,1 +1,10 @@
+user_memory = {}
 
+def update_memory(user_id, new_data):
+    if user_id not in user_memory:
+        user_memory[user_id] = {}
+
+    user_memory[user_id].update(new_data)
+
+def get_memory(user_id):
+    return user_memory.get(user_id, {})
