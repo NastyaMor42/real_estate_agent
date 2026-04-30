15b8211db7f6c767bcf35c279e92868f93a8beb4
NastyaMor42 <88672490+NastyaMor42@users.noreply.github.com>
2026-04-30 18:20:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,14 @@ USER_ID = "nastya"
 
 st.title("בדיקת MEMORY")
 
-# קלט מהמשתמש
 city = st.text_input("עיר:")
 price = st.number_input("מחיר מקסימלי:", step=500)
 
-# כפתור שמירה
 if st.button("שמור"):
     update_memory(USER_ID, {
         "city": city,
         "max_price": price
     })
 
-# הצגת הזיכרון
 st.write("הזיכרון שלך:")
 st.write(get_memory(USER_ID))
