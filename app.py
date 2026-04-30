8143c7bd53f1403b3d181fa3ffe62f9f5d3f1afd
NastyaMor42 <88672490+NastyaMor42@users.noreply.github.com>
2026-04-30 18:20:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import streamlit as st
+from memory import get_memory, update_memory
 
+USER_ID = "nastya"
+
+st.title("בדיקת MEMORY")
+
+# קלט מהמשתמש
+city = st.text_input("עיר:")
+price = st.number_input("מחיר מקסימלי:", step=500)
+
+# כפתור שמירה
+if st.button("שמור"):
+    update_memory(USER_ID, {
+        "city": city,
+        "max_price": price
+    })
+
+# הצגת הזיכרון
+st.write("הזיכרון שלך:")
+st.write(get_memory(USER_ID))
